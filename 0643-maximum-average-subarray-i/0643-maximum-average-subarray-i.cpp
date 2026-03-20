5367b524bf908e9a1ebf584543b006431d18a9dd
jnanadeep-2003 <jnanadeep02062003@gmail.com>
2026-03-20 18:01:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,52 +1,17 @@
-// class Solution {
-// public:
-//     double findMaxAverage(vector<int>& nums, int k) 
-//     {
-//         int i=0;
-//         int j=k-1;
-//         int n = nums.size()-1;
-//         int maxSum = 0;
-
-//         for(int z=0 ; z<=j ; z++)
-//             maxSum += nums[z];
-        
-//         i++;  j++;       
-//         int sum= 0;
-//         while(j <= n)
-//         {
-//             sum = sum + nums[j] - nums[i - 1];
-//             maxSum = max(maxSum, sum);
-//             i++;
-//             j++;
-//         }
-
-//         cout<<maxSum;
-
-//         return (double)maxSum/k;
-        
-//     }
-// };
-
-
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) 
     {
-        int i = 0;
-        int j = k - 1;
-        int n = nums.size() - 1;
+        int i=0;
+        int j=k-1;
+        int n = nums.size()-1;
+        int maxSum = 0;
 
-        int sum = 0;
-        for(int z = 0; z <= j; z++)
-        {
-            sum += nums[z];
-        }
-
-        int maxSum = sum;
-
-        i++;
-        j++;
-
+        for(int z=0 ; z<=j ; z++)
+            maxSum += nums[z];
+        
+        i++;  j++;       
+        int sum= maxSum;
         while(j <= n)
         {
             sum = sum + nums[j] - nums[i - 1];
@@ -55,7 +20,42 @@ public:
             j++;
         }
 
-        return (double)maxSum / k;
+        cout<<maxSum;
+
+        return (double)maxSum/k;
+        
     }
 };
+
+
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& nums, int k) 
+//     {
+//         int i = 0;
+//         int j = k - 1;
+//         int n = nums.size() - 1;
+
+//         int sum = 0;
+//         for(int z = 0; z <= j; z++)
+//         {
+//             sum += nums[z];
+//         }
+
+//         int maxSum = sum;
+
+//         i++;
+//         j++;
+
+//         while(j <= n)
+//         {
+//             sum = sum + nums[j] - nums[i - 1];
+//             maxSum = max(maxSum, sum);
+//             i++;
+//             j++;
+//         }
+
+//         return (double)maxSum / k;
+//     }
+// };
 
