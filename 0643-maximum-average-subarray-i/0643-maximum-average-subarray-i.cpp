115b5408bc93f240e0537e2e94295e2fcf0b1cc5
jnanadeep-2003 <jnanadeep02062003@gmail.com>
2026-03-20 18:00:21 +0530

    Time: 0 ms (100%), Space: 113.8 MB (27.5%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,61 @@
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& nums, int k) 
+//     {
+//         int i=0;
+//         int j=k-1;
+//         int n = nums.size()-1;
+//         int maxSum = 0;
+
+//         for(int z=0 ; z<=j ; z++)
+//             maxSum += nums[z];
+        
+//         i++;  j++;       
+//         int sum= 0;
+//         while(j <= n)
+//         {
+//             sum = sum + nums[j] - nums[i - 1];
+//             maxSum = max(maxSum, sum);
+//             i++;
+//             j++;
+//         }
+
+//         cout<<maxSum;
+
+//         return (double)maxSum/k;
+        
+//     }
+// };
+
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) 
+    {
+        int i = 0;
+        int j = k - 1;
+        int n = nums.size() - 1;
+
+        int sum = 0;
+        for(int z = 0; z <= j; z++)
+        {
+            sum += nums[z];
+        }
+
+        int maxSum = sum;
+
+        i++;
+        j++;
+
+        while(j <= n)
+        {
+            sum = sum + nums[j] - nums[i - 1];
+            maxSum = max(maxSum, sum);
+            i++;
+            j++;
+        }
+
+        return (double)maxSum / k;
+    }
+};
+
